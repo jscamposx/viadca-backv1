47e79aec4f695986849e658a506a026c5ed45b0d
jscamposx <jscamposx@gmail.com>
2025-07-26 08:24:59 -0700

    dejando estructura del proyecto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 // En src/app.module.ts
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -8,8 +9,25 @@ import { AppService } from './app.service';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      // Aquí está la magia ✨
       envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+
+    // Configuración de TypeORM
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: +(configService.get<number>('DB_PORT') ?? 3306),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        
+
+        synchronize: process.env.NODE_ENV === 'dev',
+      }),
     }),
   ],
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as compression from 'compression'; // Importa la librería
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilita la compresión
+  app.use(compression());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
